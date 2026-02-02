7e920a3b93f9fde94748d1bbb040ec283e6def86
huny <sehoon99@naver.com>
2026-02-02 17:15:27 +0900

    Initial commit

---
+++ src/test/java/com/example/ainews/AinewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ainews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AinewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
